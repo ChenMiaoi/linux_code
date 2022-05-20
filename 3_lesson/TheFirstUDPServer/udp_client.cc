559409f1ab2e69c75153d768ab1a43f75184dd6a
ChenMiaoi <3026066798@qq.com>
2022-05-20 23:07:12 +0800

    The Udp Client

--- 3_lesson/TheFirstUDPServer/udp_client.cc
+++ 3_lesson/TheFirstUDPServer/udp_client.cc
@@ -1,6 +1,15 @@
 #include "udp_client.hpp"
 
-int main(){
-    UdpClient ucli;
+//./udp_client server_ip  server_port
+
+int main(int argc, char* argv[]){
+    if (argc != 3){
+        std::cerr << "Usage: " << argv[0] << "server_ip server_port" << std::endl;
+    }
+    std::string ip = argv[1];
+    int port = atoi(argv[2]);
+    UdpClient* ucli = new UdpClient(ip, port);
+    ucli->InitUdpClient();
+    ucli->Start();
     return 0;
 }
