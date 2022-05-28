78672ac37792d59794dbf79407b7d10589abb403
ChenMiaoi <3026066798@qq.com>
2022-05-28 14:42:58 +0800

    The TCP Client by Thread Pool

--- 3_lesson/TheFirstTCPServer/tcp_sevrer.hpp
+++ 3_lesson/TheFirstTCPServer/tcp_sevrer.hpp
@@ -11,12 +11,26 @@
 #include <sys/socket.h>
 #include <sys/wait.h>
 #include <signal.h>
+#include "ThreadPool.hpp"
+#include "Task.hpp"
 
 #define BACKLOG 5
 
+struct Prama{
+    Prama(int sock, std::string ip, int port)
+        : _sock(sock)
+        , _ip(ip)
+        , _port(port)
+    {}
+    ~Prama(){}
+    int _sock;
+    std::string _ip;
+    int _port;
+};
+
 class TcpServer{
 public:
-    TcpServer(int port) : _port(port), _sock_listen(-1)
+    TcpServer(int port) : _port(port), _sock_listen(-1), tp(nullptr)
     {}
 
     ~TcpServer(){
@@ -50,10 +64,23 @@ public:
             perror("listen");
             return false;
         }
+        tp = new ThreadPool<Task> ();
         return true;
     }
+#if 0
+    static void* HanderRequest(void* arg){
+        Prama *p = (Prama*)arg;
+        pthread_detach(pthread_self());
+        Service(p->_sock, p->_ip, p->_port);
+        close(p->_sock);
+        delete p;
+        return nullptr;
+    }
+#endif
+
     void Loop(){
         //signal(SIGCHLD, SIG_IGN); 第一种方法
+        tp->ThreadPoolInit();
         while (true){
             struct sockaddr_in peer;
             socklen_t len = sizeof(peer);
@@ -63,7 +90,19 @@ public:
                 perror("accept");
                 continue;
             }
+            std::string ip = inet_ntoa(peer.sin_addr);
+            int port = ntohs(peer.sin_port);
+            Task t(sock, ip, port);
+            tp->Put(t);
+#if 0
+            pthread_t tid;
+            std::string ip = inet_ntoa(peer.sin_addr);
+            int port = ntohs(peer.sin_port);
+            Prama *p = new Prama(sock, ip, port);
+            pthread_create(&tid, nullptr, HanderRequest, p);
+#endif // !0
 
+#if 0
             pid_t id = fork();
             if (id == 0){   //child
                 close(_sock_listen);    //不必要必须关闭，一般建议关闭
@@ -79,9 +118,12 @@ public:
             //waitpid();  //是阻塞的
             close(sock);    //必须关闭
             waitpid(id, nullptr, 0);
+#endif // !0 多进程方案
         }
     }
-    void Service(int sock, const std::string& ip, const int port){
+
+#if 0
+    static void Service(int sock, const std::string& ip, const int port){
         char buffer[1024] = {};
         while (true){
             ssize_t size = read(sock, buffer, sizeof(buffer) - 1);
@@ -97,12 +139,14 @@ public:
                 perror("read");
             }
         }
-        close(sock);
         std::cout << "service done!" << std::endl;
     }
+#endif
+
 private:
     int _port;
     int _sock_listen;
+    ThreadPool<Task> *tp;
 };
 
 #endif //THEFIRSTTCPSERVER_TCP_SEVRER_HPP
